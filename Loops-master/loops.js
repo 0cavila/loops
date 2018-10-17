5ca41cca4ba043c1d66e54a0cacf055c0f9bedf0
0cavila <42749704+0cavila@users.noreply.github.com>
2018-10-17 14:02:46 -0500

    Add files via upload

---
+++ Loops-master/loops.js
@@ -0,0 +1,29 @@
+// For-loop functions
+
+function positiveIntSum(/* Give it one input */){
+    let sum = 0 
+    for(let i = 1; i <= limit; i ++) 
+
+
+}
+
+
+function additionPatternSum(/* Give it three inputs */){
+
+}
+
+
+
+
+// While-loop function
+
+function multiplicationPatternSum(/* Give it three inputs */){
+
+
+}
+
+
+
+
+
+//Later copy paste the checking code below here.
