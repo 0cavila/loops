22eff87f43dd78429f35573f012c2e5ffd3304cb
0cavila <42749704+0cavila@users.noreply.github.com>
2018-10-22 13:56:59 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,37 +1,47 @@
-// For-loop functions
-
 function positiveIntSum(limit){
     let sum = 0;
-    let n = 100;
-    for (let i= 1; i<= limit ; i ++){
-    sum = sum + i ;
-}
-return sum;
-
-
-function additionPatternSum(start diff,limit){
-let start ; let diff ; 
-for (let i = start; let i<= limit ; i = i + diff){
-    sum = sum + i;
-return diff
-}
- 
-
-
-
-// While-loop function
-
-function multiplicationPatternSum(star, factor, max){
-let sum = start 
-let k=1 
-while (k <= max)
-sum=  sum + factor*k;
-k= factor*k 
-
+    for(let k=1; k<=limit; k++){
+        sum = sum + k;
+    }
+    return sum;
 }
 
 
+function addSum(start, diff, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
 
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber + diff;
+    }
 
+    return sum;
+}
 
-//Later copy paste the checking code below here.
+function multSum(start, factor, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
+
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber * factor;
+    }
+
+    return sum;
+}
+console.log(positiveIntSum(1500));
+console.log(positiveIntSum(2000000));
+
+console.log(addSum(-80, 60, 2860));
+console.log(addSum(7, 7, 50000));
+
+queston two was larger was larger cause the outcome was 2000000
+console.log(multSum(4, 3, 500000));
+console.log(multSum(2, -3, 1000000));
